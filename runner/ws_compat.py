1356a0b0169ac9ba6300b1100500411831858b09
TheCinematicTravellers <71020068+TheCinematicTravellers@users.noreply.github.com>
2026-09-03 13:17:23 +0530

    Add SmartAPI data callback adapter

--- runner/ws_compat.py
+++ runner/ws_compat.py
@@ -1,2 +1,6 @@
 def close_callback(sdk_on_close, wsapp, close_status_code=None, close_msg=None):
     sdk_on_close(wsapp)
+
+
+def data_callback(runner_on_data, wsapp, data, data_type=None, continue_flag=None):
+    runner_on_data(data)
